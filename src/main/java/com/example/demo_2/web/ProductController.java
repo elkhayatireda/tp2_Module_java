9d239c7f5796bbd1304c968d4801de981b03c603
elkhayatireda <elkhayatireda2@gmail.com>
2026-03-13 23:44:05 +0000

    layout + add new product

--- src/main/java/com/example/demo_2/web/ProductController.java
+++ src/main/java/com/example/demo_2/web/ProductController.java
@@ -2,10 +2,14 @@ package com.example.demo_2.web;
 
 import com.example.demo_2.entities.Product;
 import com.example.demo_2.repository.ProductRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -19,5 +23,29 @@ public class ProductController {
         List<Product> products =  productRepository.findAll();
         model.addAttribute("productList" , products);
         return "products";
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "redirect:/index";
+    }
+
+    @GetMapping("/newProduct")
+    public String newProduct(Model model) {
+        model.addAttribute("product", new Product());
+        return "new-product";
+    }
+
+    @GetMapping("/delete")
+    public String delete(@RequestParam(name = "id") Long id){
+        productRepository.deleteById(id);
+        return "redirect:/index";
+    }
+
+    @PostMapping("/saveProduct")
+    public String saveProduct(@Valid Product product, BindingResult bindingResult, Model model) {
+        if(bindingResult.hasErrors()) return "new-product";
+        productRepository.save(product);
+        return "redirect:/newProduct";
     }
 }
